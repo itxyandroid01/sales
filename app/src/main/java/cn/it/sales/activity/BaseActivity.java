96c533785725cb8de14b579bad520c6992ea53bf
maohl <maohl@163.com>
2016-06-02 20:28:08 +0800

    添加了一行注释

--- app/src/main/java/cn/it/sales/activity/BaseActivity.java
+++ app/src/main/java/cn/it/sales/activity/BaseActivity.java
@@ -9,7 +9,7 @@ import cn.it.sales.application.MyApplication;
  * Created by Administrator on 2016/6/1.
  */
 public class BaseActivity extends AppCompatActivity{
-    //这是一个注释 _mmmm
+    //我添加了一行注释
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
