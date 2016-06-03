a1f4e4b3bf324b684f7ae62f2c24a473018a7ba8
sk43too <757041071@qq.com>
2016-06-03 10:26:08 +0800

    修改BaseActivity的提示

--- app/src/main/java/cn/it/sales/activity/BaseActivity.java
+++ app/src/main/java/cn/it/sales/activity/BaseActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import cn.it.sales.application.MyApplication;
 
 /**
- * Created by Administrator on 2016/6/1.
+ * Created by Administrator on 2016/6/3.
  * 本Activity是其他Activity的基类
  */
 public class BaseActivity extends AppCompatActivity{
