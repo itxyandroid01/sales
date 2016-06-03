ea7b57be77a606ed07e932d90c8bd67a6529237d
sk43too <757041071@qq.com>
2016-06-03 10:23:55 +0800

    修改BaseActivity的提示

--- app/src/main/java/cn/it/sales/activity/BaseActivity.java
+++ app/src/main/java/cn/it/sales/activity/BaseActivity.java
@@ -7,17 +7,21 @@ import cn.it.sales.application.MyApplication;
 
 /**
  * Created by Administrator on 2016/6/1.
+ * 本Activity是其他Activity的基类
  */
 public class BaseActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //添加本Activity到列表中
         MyApplication.getMyApplication().addActivity(this);
     }
 
     @Override
     public void finish() {
+        //从列表中移除本Activity
         MyApplication.getMyApplication().removeActivity(this);
+
         super.finish();
     }
 }
